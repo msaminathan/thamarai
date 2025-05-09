b5f9c18ca4bc652153aa3a47f3886eb0f773c06f
msaminathan <108092378+msaminathan@users.noreply.github.com>
2025-05-09 17:34:49 -0400

    Update app_dow_csv.py

--- app_dow_csv.py
+++ app_dow_csv.py
@@ -988,11 +988,6 @@ elif page == "Interactive Portfolio Optimizer" and opt == True:
         plt.ylabel("Portfolio Growth(%)", fontsize=7)
         plt.xticks(fontsize=7)
         plt.yticks(fontsize=6)	    
-        #plt.figure(figsize=(10,5))
-        #plt.title("Porfolio Growth(%) - Since "  + str(prices2.index[0]))
-        #plt.grid(True)
-        #plt.xlabel("Time")
-        #plt.ylabel("Portfolio Growth(%)")
         plt.plot(prices2.index, 100.0*(np.dot(prices2, nshares) / initial_porfolio - 1.0), label='Maximum Sharpe Ratio')
         nshares = weights_df.iloc[int(min_index)]/prices2.iloc[0]
         initial_porfolio = np.dot(prices2.iloc[0], nshares)         
