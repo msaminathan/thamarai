1ade53ba0593ad50a2f9ca152a93492e2b220577
msaminathan <108092378+msaminathan@users.noreply.github.com>
2025-05-11 07:39:51 -0400

    Update app_dow_csv.py

--- app_dow_csv.py
+++ app_dow_csv.py
@@ -999,7 +999,7 @@ elif page == "Interactive Portfolio Optimizer" and opt == True:
         prices.plot(linewidth=0.5, title="Stocks History", grid=True)
         plt.xlabel("Date", fontsize=6)
         plt.xticks(fontsize=6)
-        plt.ylabel(fontsize=6)
+        plt.ylabel("Price", fontsize=6)
         plt.yticks(fontsize=6)
         st.pyplot(plt)
         st.write("Stocks Price History : " + str(start_date) + " to " + str(end_date))
